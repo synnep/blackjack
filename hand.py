d3c0080bf14356a0eecbc794d63a230a70d6f5f6
Synne <113035984+synnep@users.noreply.github.com>
2022-11-20 14:28:23 +0100

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,34 +1,48 @@
-# Ikke fullstendig!!
-
 from kortstokk import Kortstokk
 
 kortstokk = Kortstokk()
 kortstokk.visKortstokkInfo()
 
 class Hand():
+    """
+    Klasse for å representere en hånd med kort
+
+    Bruker en ferdig stokket kortstokk for å fylle hånden med kort
+        
+    """
     
     def __init__(self):
         self.kortPaHand = []
         self.genererHand()
     
     def genererHand(self):
+        """
+        genererer en hånd med to kort
+            de to øverste kortene fra en ferdig stokket kortstokk
+        """
         for i in range(2):
             self.kortPaHand.append(kortstokk.trekk())
     
     def visHand(self):
+        """
+        viser kortene hånden sitter på
+        """
         for kort in self.kortPaHand:
             kort.visKortInfo()
     
     def finnSumm(self):
+        """
+        finner summen til kortene hånden besitter
+        tar hensyn til at ess kan ha flere verdier
+        !! returnerer ingen verdi enn så lenge, kun print
+        """
         ess = "nei"
+
         for kort in self.kortPaHand:
                 if kort.tall == 13:
                     ess = "ja"
-                else:
-                    print("Det er ikke et ess")
         
         if ess == "ja":
-            print("Det er et ess")
             s1 = 0
             s2 = 0
            
@@ -41,24 +55,12 @@ class Hand():
                     s2 += 1
                 else:
                     s2 += kort.tall
+                
             print("Sum med 13: ", s1)
             print("Sum med 1: ", s2)
+
         else: 
             s = 0
             for kort in self.kortPaHand:
                 s += kort.tall
             print(s)
-
-
-hand1 = Hand()
-print()
-print(hand1)
-print()
-print(hand1.visHand())
-
-print(len(hand1.kortPaHand))
-print()
-
-hand1.finnSumm()
-
-
